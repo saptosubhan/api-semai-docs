a7f02c0b18bfbb6efbf803b178b24ae30dd179af
Sapto Subhan <saptosubhan@gmail.com>
2025-08-11 08:59:37 +0700

    add new API

--- # API generate FILES.rb
+++ # API generate FILES.rb
@@ -1,12 +1,12 @@
 # API generate FILES
 
-## SUCOFINDO -> SEMAI
+## BPDP -> SEMAI
 ### Request
 {
     file_id: 1, # ID file upload di database
     file_url: "https://example.com/storage/uploads/rekening-koran/evidence/file.xlsx", # URL untuk mengakses file
     category: "rekening_koran", # Kategori file upload, contoh "rekening_koran", "master_hpe", "keberatan_sp3es", "restitusi"
-    request_id: "a12f6b1e-4d98-4b22-bc41-2f6b9c3e8e9f" # UUID dari SUCOFINDO
+    request_id: "RAND-UUID" # UUID dari BPDP
 }
 ### Response
 {
@@ -16,11 +16,11 @@
 }
 
 
-## SEMAI -> SUCOFINDO
+## SEMAI -> BPDP
 ### Request
 {
     file_id: 123,
-    request_id: "a12f6b1e-4d98-4b22-bc41-2f6b9c3e8e9f", # UUID dari SEMAI
+    request_id: "RAND-UUID", # UUID dari SEMAI
     category: "rekening_koran", # Kategori file upload, contoh "rekening_koran", "master_hpe", "keberatan_sp3es", "restitusi"
     row_count: 20, # jumlah rows data (khusus untuk bulk insert)
     rows: [] # isi data dalam bentuk arrays
@@ -34,13 +34,30 @@
 
 
 # API tarif pungutan
-#Request
+##Request tarif pungutan BPDP ke SEMAI
 {
-    request_id: "a12f6b1e-4d98-4b22-bc41-2f6b9c3e8e9f", # UUID dari SUCOFINDO
-    category: "rekening_koran",
+    request_id: "RAND-UUID", # UUID dari BPDP
+    category: "tarif_pungutan",
+    uraian_lhp: "",
+    tarif_pungutan: "",
     
 }
-#Response   
+##Response tarif pungutan BPDP ke SEMAI
+{
+    status: 200, 403, dll..
+    message: success, failed, error, etc..
+    data : {} # optional jika ada               
+}
+
+##Request tarif pungutan SEMAI ke BPDP
+{
+    request_id: "RAND-UUID", # UUID dari SEMAI
+    category: "tarif_pungutan",
+    kelompok_tarif: "I,II,III,IV,etc...",
+    nilai_tarif: "20, 7.5, 5,etc...",
+    satuan_nilai_tarif: "percent, usd"
+}
+##Response tarif pungutan SEMAI ke BPDP
 {
     status: 200, 403, dll..
     message: success, failed, error, etc..
